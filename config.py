4fc55dda2d5fa0c99a9b4558bd5eff263e4258bb
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-04 01:07:02 +0430

    config File

---
+++ config.py
@@ -0,0 +1,9 @@
+USERNAME = "koosha"
+PASSWORD = "102030"
+ADMIN_USERNAME="admin"
+ADMIN_PASSWORD="admin"
+DB_HOST = "127.0.0.1"
+DB_USER = "root"
+DB_PASSWORD = "Koosha_102030"
+DB = "mapper" 
+DB_PORT=3360
