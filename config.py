b3b067244b0887978cd8a7e8bced6c8aa54a4be8
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-05 14:21:41 +0430

    add remote host config

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 USERNAME = "koosha"
 PASSWORD = "102030"
-ADMIN_USERNAME="admin"
-ADMIN_PASSWORD="admin"
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
+
 DB_HOST = "127.0.0.1"
+DB_REMOTE_HOST = "127.0.0.1"
+
 DB_USER = "root"
+DB_REMOTE_USER = "root"
+
 DB_PASSWORD = "Koosha_102030"
-DB = "mapper" 
-DB_PORT=3360
+DB_REMOTE_PASSWORD = "Koosha_102030"
+
+DB = "mapper"
+DB_PORT = 3360
