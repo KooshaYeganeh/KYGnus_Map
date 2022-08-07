75965747aa84bab3c162803fb41d110df9fd76a7
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-07 16:32:27 +0430

    edit port

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ DB_REMOTE_HOST = "127.0.0.1"
 DB_USER = "user of Database"
 DB_REMOTE_USER = "User of remote system"
 
-DB_PASSWORD = "Password of Database"
+DB_PASSWORD = "Password of mariadb"
 DB_REMOTE_PASSWORD = "Password of Remote Server Database"
 
 DB = "mapper"
-DB_PORT = Port of mariaDB
+DB_PORT = 3306
