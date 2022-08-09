29a7f1864255f734fc21529ced9a437663727b31
kooshakooshadv <kooshakooshadv@gmail.com>
2022-08-09 15:01:54 +0430

    add comments to config File

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ ADMIN_PASSWORD = "admin"
 DB_HOST = "127.0.0.1"
 DB_REMOTE_HOST = "127.0.0.1"
 
-DB_USER = "user of Database"
+DB_USER = "Database User" 
 DB_REMOTE_USER = "User of remote system"
 
-DB_PASSWORD = "Password of mariadb"
+DB_PASSWORD = "Database password" 
 DB_REMOTE_PASSWORD = "Password of Remote Server Database"
 
 DB = "mapper"
-DB_PORT = 3306
+DB_PORT = 3360
