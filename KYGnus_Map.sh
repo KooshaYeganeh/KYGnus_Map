021028e4ad9d51efa07b69829209df5138776090
kooshakooshadv <kooshakooshadv@gmail.com>
2022-11-26 14:27:11 +0330

    add more Linux Distributions for Run File

--- KYGnus_Map.sh
+++ KYGnus_Map.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/bash
 
 
+cd /opt/KYGnus_Map
 
-username=`users | cut -d" " -f1`
+
+# In some Linux distributions, the command to run Python may be different. For example
+# Ubuntu : python3 app.py
+# peppermint : python3 app.py
+# OpenSuse : python3 app.py
+# Fodora : python app.py
+## If there is a problem in running the program, one of the parts that you
+##should consider in troubleshooting is changing the Python execution command
+
+Fedora=` cat /etc/os-release | grep -o fedora | head -1`
+Ubuntu=` cat /etc/os-release | grep -o fedora | head -1`
+OpenSuse=` cat /etc/os-release | grep -o fedora | head -1`
 
 
-cd /home/$username/Apps/KYGnus_Map
+if [ "$Fedora" == "fedora" ];then
+	sudo python app.py
 
-source venv/bin/activate
+elif [ "$Ubuntu" == "ubuntu" ];then
+	sudo python3 app.py
 
-python app.py
+elif [ "$OpenSuse" == "opensuse" ];then
+	sudo python3 app.py
+fi
+
+
 
