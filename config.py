29bc6182cab94af62d7797cae8f741d1fdb613ae
kooshakooshadv <kooshakooshadv@gmail.com>
2022-11-26 14:19:12 +0330

    add comments to config file

--- config.py
+++ config.py
@@ -1,16 +1,24 @@
+# Username of App
 USERNAME = "koosha"
+# Password of App
 PASSWORD = "102030"
+# Admin Page username
 ADMIN_USERNAME = "admin"
+# Admin Page Passoword
 ADMIN_PASSWORD = "admin"
 
+# Host Address
+# If your database server is separate from the application server
+#you must enter the IP of the database server in this field
 DB_HOST = "127.0.0.1"
-DB_REMOTE_HOST = "127.0.0.1"
 
+# Database User Like root or others
 DB_USER = "Database User" 
-DB_REMOTE_USER = "User of remote system"
-
 DB_PASSWORD = "Database password" 
-DB_REMOTE_PASSWORD = "Password of Remote Server Database"
 
+# Database Name
 DB = "mapper"
+# In this section, enter the port number on which mariadb is running.
+# or security reasons, it is better to change the mariadb port number 
+# from the default 3306. You can do this from your mariadb settings.
 DB_PORT = 3360
